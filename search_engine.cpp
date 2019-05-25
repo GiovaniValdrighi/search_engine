1d3b5343987b83295274449f363dd838f9443fe2
FredsoNerd <fredsonerd@hotmail.com>
2019-05-25 16:06:48 -0300

    Funcao search_keys atualizada para busca de varios termos

--- search_engine.cpp
+++ search_engine.cpp
@@ -10,7 +10,7 @@ struct Node{
 	// "int *pages" é ponteiro para o primeiro id do array
 	int *pages;
 	int size;
-	Node *pChild[27];
+	Node *pChild[37];
 	
 	Node(){
 		for(int i = 0; i < 26; i++) pChild[i] = nullptr; 
@@ -19,7 +19,7 @@ struct Node{
 	int getIndex(char c){
 		// cada letra "c" retorna o índice no array
 		int index = 0;
-		for(char i: "abcdefghijklmnopqrstuvwxyz"){
+		for(char i: "aeioubcdfghjklmnpqrstvwxyz0123456789"){
 			if(c == i) return index;
 			index += 1;
 		}
@@ -79,26 +79,51 @@ class Trie{
 		(*pNode)->size = size;
 		cout << "Inserindo " << key << endl;
 	}
-	void search_keys(string *keys, int size){
-		// "keys" uma lista de palavras requisitadas
-		// "size" é o numero de palavras na requisicao
-		int *res;
-		int len_res;
-		search(keys[0], len_res, res);
-		open_pages(res, len_res);
-		for(int i = 1; i < size; i++){
-			// desejamos receber o retono e fazer a
-			// intercessao eficiante entre as listas
-			// supomos as listas de inteiros ordenadas
-			
+	void intersection(int *&A, int &len_a, int *B, int len_b){
+		// interceccao entre as listas A e B
+		int len_r = 0;
+		
+		int i = 0;
+		int j = 0;
+		while(i < len_a && j < len_b){
+			if(A[i] == B[j]){
+				A[len_r] = A[i];
+				
+				i++;
+				j++;
+				len_r++;
+			}
+			else if(A[i] > B[j]) j++;
+			else i++;
 		}
+		len_a = len_r;
+	}
+	void search_keys(string *keys, int size){
+		// "keys" uma lista das palavras requisitadas
+		// "size" é numero de palavras na requisicao
+		int *res;
+		int len_r;
+		search(keys[0], len_r, res);
+		
+		int *aux;
+		int len_a;
+		string key;
+		for(int i = 1; i < size; i++){
+			// interseccionamos entre listas a cada laço
+			// supomos as listas de inteiros ordenadas
+			key = keys[i];
+			search(key, len_a, aux);
+			
+			intersection(res, len_r, aux, len_a);
+		}
+		open_pages(res, len_r);
 	}
 	void search(string key, int &len, int *&res){
 		// "string key" é a palavra buscada
 		// buscamos iteradamente até o ultimo node
 		Node *pNode = pRoot;
 		for(char c: key){
-			pNode = pNode->pChild[pNode->getIndex(c)]; //caminho até o último nó
+			pNode = pNode->pChild[pNode->getIndex(c)];
 		}
 		cout << "Pegando em node: " << key << endl;
 		
@@ -120,15 +145,8 @@ class Trie{
 int main(){
 	Trie trie = Trie("teste.txt");
 	//open_file("teste.txt", trie);
-	string s[3] = {"giovani"};
-	trie.search_keys(s, 1);
+	string s[4] = {"outra", "giovani", "etecetera", "poco2"};
+	trie.search_keys(s, 4);
 	
-	//string input;
-	//while(1){
-	//	cout << endl << "\nEnter your query (0 - exit): ";
-	//	cin >> input;
-	//	if(input == "0") break;
-	//	trie.search(input);
-	//}
 	return 0;
 }
